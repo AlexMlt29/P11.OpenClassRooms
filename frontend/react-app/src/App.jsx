c653e6824b1b380330cea50cc7d8c07bb7ce4fc5
AlexMlt29 <alexandre.millet29600@gmail.com>
2024-06-11 15:35:11 +0200

    Adding mockup HTML into a React CRA (Create React App)

---
+++ frontend/react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignInPage from "./pages/SignInPage";
+import './App.css';
+
+function App() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/SignInPage" element={<SignInPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+  
+export default App;
---
+++ frontend/react-app/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../logos/argentBankLogo.png";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header>
+        <nav className="main-nav">
+          <Link to="/" className="main-nav-logo">
+            <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
+            <h1 className="sr-only">Argent Bank</h1>
+          </Link>
+          <div>
+            <Link to="/SignInPage" className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              Sign In
+            </Link>
+          </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Header;
