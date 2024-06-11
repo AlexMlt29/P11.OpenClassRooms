8f5eae0546ca95578a0693eeb19557a9a8252b90
AlexMlt29 <alexandre.millet29600@gmail.com>
2024-06-11 18:46:07 +0200

    Adding SignInPage.jsx mockup from sign-in.html

--- frontend/react-app/src/components/Header.jsx
+++ frontend/react-app/src/components/Header.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import logo from "../logos/argentBankLogo.png";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
-    return (
-        <header>
-        <nav className="main-nav">
-          <Link to="/" className="main-nav-logo">
-            <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
-            <h1 className="sr-only">Argent Bank</h1>
+  return (
+    <header>
+      <nav className="main-nav">
+        <Link to="/" className="main-nav-logo">
+          <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+        <div>
+          <Link to="/SignInPage" className="main-nav-item">
+            <FontAwesomeIcon icon={faUserCircle} className="fa fa-user-circle" />
+            Sign In
           </Link>
-          <div>
-            <Link to="/SignInPage" className="main-nav-item">
-              <i className="fa fa-user-circle"></i>
-              Sign In
-            </Link>
-          </div>
-        </nav>
-      </header>
-    );
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
--- frontend/react-app/src/pages/SignInPage.jsx
+++ frontend/react-app/src/pages/SignInPage.jsx
@@ -1,35 +1,31 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 function SignInPage() {
   return (
-    <div>
+    <div className="app-container">
       <Header />
-      <main class="main bg-dark">
-        <section class="sign-in-content">
-          <i class="fa fa-user-circle sign-in-icon"></i>
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <FontAwesomeIcon icon={faUserCircle}/>
           <h1>Sign In</h1>
           <form>
-            <div class="input-wrapper">
-              <label for="username">Username</label>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
               <input type="text" id="username" />
             </div>
-            <div class="input-wrapper">
-              <label for="password">Password</label>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
               <input type="password" id="password" />
             </div>
-            <div class="input-remember">
+            <div className="input-remember">
               <input type="checkbox" id="remember-me" />
-              <label for="remember-me">Remember me</label>
+              <label htmlFor="remember-me">Remember me</label>
             </div>
-            {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-            <a href="./user.html" class="sign-in-button">
-              Sign In
-            </a>
-            {/* <!-- SHOULD BE THE BUTTON BELOW -->
-            <!-- <button class="sign-in-button">Sign In</button> -->
-            <!--  --> */}
+            <button className="sign-in-button">Sign In</button>
           </form>
         </section>
       </main>
