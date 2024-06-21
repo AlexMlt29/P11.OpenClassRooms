1444f0d3f91a203ff6e8a382bf862b970a32f22c
AlexMlt29 <alexandre.millet29600@gmail.com>
2024-06-21 10:18:48 +0200

    .

--- frontend/react-app/src/index.jsx
+++ frontend/react-app/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/store'; // Assurez-vous que ce chemin est correct
+import store from './redux/store';
 import './App.css';
 import App from './App';
 
