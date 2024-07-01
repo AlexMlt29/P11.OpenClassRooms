106a254b83412041dbc08363c1061241862101fb
AlexMlt29 <alexandre.millet29600@gmail.com>
2024-07-02 01:35:33 +0200

    Adding new logic on AuthPage with redux to state logged in or logged out to show form or not

--- frontend/react-app/src/pages/AuthPage/AuthPage.jsx
+++ frontend/react-app/src/pages/AuthPage/AuthPage.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import AuthForm from "../../components/Authentification/AuthForm";
 import "./AuthPage.css";
 
 const AuthPage = () => {
+  const token = useSelector((state) => state.auth.token);
+
   return (
     <div className="app-container">
       <main className="main bg-dark">
         <section className="sign-in-content">
-          <FontAwesomeIcon icon={faUserCircle}/>
-          <h1>Sign In</h1>
-          <AuthForm />
+          {!token ? (
+            <>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <h1>Sign In</h1>
+              <AuthForm />
+            </>
+          ) : (
+            <h1>You are already logged in !</h1>
+          )}
         </section>
       </main>
     </div>
